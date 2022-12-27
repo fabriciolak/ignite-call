37981a06c998f00fc104be988c02667227e203c5
Fabrício <fabriciolak12@gmail.com>
2022-12-27 16:15:22 -0300

    initial setup

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import { Heading, Text } from '@ignite-ui/react'
+
+export default function Home() {
+  return (
+    <div>
+      <Heading>Hello world</Heading>
+
+      <Text>Hello</Text>
+    </div>
+  )
+}
