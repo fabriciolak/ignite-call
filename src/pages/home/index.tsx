e256ee37ae0a67ecd59c510690c1242a45ca2445
Fabrício <fabriciolak12@gmail.com>
2022-12-27 17:54:28 -0300

    refactor(pages): restyle home to improve responsibility to mobile devices

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,30 @@
+import Image from 'next/image'
+
 import { Heading, Text } from '@ignite-ui/react'
+
+import { Container, Hero, Preview } from './styles'
+import previewImage from '../../assets/app-preview.png'
 
 export default function Home() {
   return (
-    <div>
-      <Heading>Hello world</Heading>
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
 
-      <Text>Hello</Text>
-    </div>
+      <Preview>
+        <Image
+          src={previewImage}
+          alt="Calendário simbolizando a aplicação"
+          height={442}
+          quality={100}
+          priority
+        />
+      </Preview>
+    </Container>
   )
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,7 +1,28 @@
 import { styled } from '@ignite-ui/react'
 
-export const Container = styled('div', {})
+export const Container = styled('div', {
+  maxWidth: 'calc(100vw - (100vw - 1160px) / 2)',
+  height: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginLeft: 'auto',
+  gap: 96,
 
-export const Hero = styled('div', {})
+  '@media (min-width: 1440px)': {
+    margin: '0 auto',
+    overflow: 'visible',
+  },
+})
 
-export const Preview = styled('div', {})
+export const Hero = styled('div', {
+  marginLeft: 120,
+})
+
+export const Preview = styled('div', {
+  overflow: 'hidden',
+
+  '@media (min-width: 1440px)': {
+    overflow: 'visible',
+  },
+})
