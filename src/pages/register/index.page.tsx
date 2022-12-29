24b69a66fab1d59f30b684c71e6a59d40187bc9b
Fabrício <fabriciolak12@gmail.com>
2022-12-29 11:26:50 -0300

    feat: check if user exists and persist user id in cookies

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -10,6 +10,7 @@ import * as z from 'zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { api } from '../../lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -48,8 +49,10 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
-    } catch (error) {
-      console.log(error)
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message)
+      }
     }
   }
 
