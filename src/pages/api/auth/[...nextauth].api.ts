c8e48cfe617d4e5ae771d00ff0f85697f4f95ed7
Fabrício <fabriciolak12@gmail.com>
2023-01-01 19:47:35 -0300

    feat: create api route for next auth

---
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -0,0 +1,14 @@
+import { NextAuthOptions } from 'next-auth'
+import NextAuth from 'next-auth/next'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+}
+
+export default NextAuth(authOptions)
